3b214c7a771463ee41e7f454364af861a370da98
Santhosh Umapathi <Santhosh.Umapathi@app-city.co>
2021-09-25 06:48:11 +0200

    FEAT: Fonts added

---
+++ src/screens/CategoryMealScreen.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+  Image,
+  FlatList,
+  Platform,
+} from "react-native";
+
+const CategoryMealScreen = (props) => {
+  const { navigation } = props;
+
+  return (
+    <View style={styles.containerView}>
+      <Text style={styles.text}>CategoryMealScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default CategoryMealScreen;
